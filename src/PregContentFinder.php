2a1cb2440c717dd17fc150cf068ff17f38d369c5
sl5net <sl5softwarelab@gmail.com>
2025-05-09 21:38:46 +0200

    feat(PCF-Refactor): Phase 1 complete - Core structure, setters, getters, and SearchMode enum implemented and tested

--- src/PregContentFinder.php
+++ src/PregContentFinder.php
@@ -55,15 +55,12 @@ if(basename($_SERVER["PHP_SELF"]) == basename(__FILE__)) {
 }
 class PregContentFinder {
     private static $selfTest_defaults = array(); # please fill that first time
-
-
     private static $selfTest_called_from_init_defaults = false;
     private static $selfTest_collection_finished = false;
     public $isUniqueSignUsed = false;
     private $content = null;
     private $regEx_begin = null;
     private $regEx_end = null;
-    
 
     private $stopIf_EndBorder_NotExistInContent = false;
     private $stopIf_BothBorders_NotExistInContent = true;
@@ -86,7 +83,7 @@ class PregContentFinder {
     public
     static $lastObject = null;
 
-    private array $foundPos_list = [];
+    private $foundPos_list;
     private $uniqueSignExtreme = null;
 
     /**
